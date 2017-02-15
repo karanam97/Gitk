e6a7f2e1c4dcb423f80948c120f413ea6d462ef6
Phani <psknfb@mail.missouri.edu>
2017-02-15 17:53:21 -0600

    MY file

---
+++ LAB1_P1.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+
+#define pi 3.14
+
+int main(void)
+{
+ float r;
+ printf("enter the radius of circle: ");
+ scanf("%f", &r);
+ printf("\ncircumference of circle is: %f",(pi*2*r));
+return 0;
+}
